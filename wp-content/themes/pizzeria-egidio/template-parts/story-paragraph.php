418005eb962e80d020d4bfa04ba13a9ed8bdd061
mattia.filosa <mattia.filosa@hibo.it>
2025-10-11 15:53:14 +0200

    fix story paragraph

--- wp-content/themes/pizzeria-egidio/template-parts/story-paragraph.php
+++ wp-content/themes/pizzeria-egidio/template-parts/story-paragraph.php
@@ -10,7 +10,7 @@ $image_story = get_field('image_story');
     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/storia-egidio-vintage.jpg" alt="Foto storica di Egidio" />
   </div>
   <div class="pizza-special-content">
-    <h2><span class="accent-font"><?php echo esc_html($title_story); ?></span> <?php echo esc_html($title_story_gold); ?></h2>
-    <p class="lead"><?php echo esc_html($text_story); ?></p>
+    <h2><span class="accent-font"><?php echo esc_html($title_story_gold); ?></span> <?php echo esc_html($title_story); ?></h2>
+    <div class="lead"><?php echo function_exists( 'wp_kses_post' ) ? wp_kses_post( $text_story ) : $text_story; ?></div>
   </div>
 </div>
