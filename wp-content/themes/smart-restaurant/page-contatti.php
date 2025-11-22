0e2fc038e9d405eccfe83985e0a6403891f82123
mattia.filosa <mattia.filosa@hibo.it>
2025-11-22 14:35:12 +0100

    Remove Blocksy theme files.

---
+++ wp-content/themes/smart-restaurant/page-contatti.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Contatti
+*/
+get_header(); ?>
+
+<main class="main-content">
+    <?php smart_restaurant_breadcrumbs(); ?>
+
+    <section class="section">
+        <div class="container">
+            <!-- page heading -->
+            <?php include get_template_directory() . '/template-parts/page-heading.php'; ?>
+
+            <!-- Layout Contatti -->
+            <?php include get_template_directory() . '/pages/page-contacts.php'; ?>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
